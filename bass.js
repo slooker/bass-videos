b2a07d56059119528089f22376c2ec1fdb59b356
Shawn Looker <slooker@curs.es>
2015-01-20 00:22:29 -0500

    Got it working, sort of... working on layout.

--- bass.js
+++ bass.js
@@ -3,8 +3,8 @@ var http = require('http');
 var fs = require('fs');
 var hb = require('handlebars');
 
+var ROW_WIDTH = 3;
 var index = fs.readFileSync('index.hbs').toString();
-
 
 var s3 = new AWS.S3(); 
 var bucket = 'bass-videos'
@@ -20,19 +20,41 @@ s3.listObjects({ Bucket: bucket }, function(err, data) {
     var videoData = data["Contents"]
     videoData.forEach(function(vid) {
       var key = vid["Key"].replace(/ /g, "+");
+      var entry = vid["Key"].split(" - ");
+      var day = "Day "+entry[0];
+      var artist = entry[1];
+      var song = entry[2].replace(".mp4", "");
+console.log("Day: "+day+", artist: "+artist+", song: "+song);
       var prettyKey = vid["Key"].replace(".mp4","");
       var url = baseUrl + bucket + '/' + key;
       //console.log(url);
-      videos.push( { key: prettyKey, url: url } );
+      videos.push( { key: prettyKey, url: url, song: song, day: day, artist: artist } );
     });
   }
   writeS3Data(videos);
 });
 
+// Transform an array into a 2d array X rows wide.
+function transform ( arr ) {
+  var result = [], temp = [];
+  arr.forEach( function ( elem, i ) {
+    if ( i > 0 && i % ROW_WIDTH === 0 ) {
+      result.push( temp );
+      temp = [];
+    }
+    temp.push( elem );
+  });
+  if ( temp.length > 0 ) {
+    result.push( temp );
+  }
+  return result;
+}
+
 function writeS3Data(videos) {
+  videos = transform(videos);
   fs.writeFile('videos.json', JSON.stringify(videos), function(err) {
     if (err) return console.log(err); 
-    console.log(JSON.stringify(videos) + ' > videos.json');
+    //console.log(JSON.stringify(videos) + ' > videos.json');
   });
 }
 
@@ -42,8 +64,8 @@ var server = http.createServer(function (request, response) {
   var template = hb.compile(index); 
   var videoFile = fs.readFileSync('videos.json');
   var videos = JSON.parse(videoFile);
-  
-//console.log(videoHash);
+
+  //console.log(videos);
   var html = template({videos: videos});
   response.end(html);
 }).listen(8000);
