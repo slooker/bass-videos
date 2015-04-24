5fd527545ebd52869eac7d0ad92ac18e219ba71f
slooker <slooker@curs.es>
2015-04-23 21:28:14 -0700

    Got artist list working.  Now to get the links for it working.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,29 @@ var Ajax = require('./ajax.min.js');
 
 var videos = [];
 
+function uniqBy(uniq, key, dedup) {
+  if (dedup == null) { dedup = true; }
+  uniq.sort(function(a,b){
+    return a[key] < b[key] ? -1 : 1;
+  });
+  //console.log(uniq);
+
+  if (dedup) { 
+    for (var i = uniq.length - 2; i >= 0; i--) {
+      if (uniq[i + 1][key] == uniq[i][key]) {
+        uniq.splice(i, 1);
+      }
+    }
+  }
+  return uniq;
+}
+
+function extractUnique(uniq, key, dedup) {
+  uniq = uniqBy(uniq, key, dedup);
+console.log(key);
+  return uniq.map(function(u) { return u[key]; });
+}
+
 var Header = React.createClass({
   render: function() {
     return (
@@ -23,10 +46,10 @@ var Nav = React.createClass({
     return (
       <div id="left-rail">
         <ul>
-          <li><a href="/">Home</a></li>
+          <li><Link to="home">Home</Link></li>
           <li><Link to="songs">Songs</Link></li>
-          <li><a href="/days">Days</a></li>
-          <li><a href="/artists">Artists</a></li>
+          <li><Link to="days">Days</Link></li>
+          <li><Link to="artists">Artists</Link></li>
         </ul>
       </div>
     );
@@ -50,9 +73,11 @@ var Box = React.createClass({
 
 var Videos = React.createClass({
   loadVideos: function() {
-    Ajax.get('/api/videos').then(function(response) {
-      this.setState({videos: response});
-    }.bind(this));
+    if (videos.length == 0) {
+      Ajax.get('/api/videos').then(function(response) {
+        this.setState({videos: response});
+      }.bind(this));
+    }
   },
   getInitialState: function() {
     return {videos: []};
@@ -65,7 +90,7 @@ var Videos = React.createClass({
       <div id="content">
         {this.state.videos.map(function(video) {
           if (!video.description)  {
-            video.description = <span><p>Day {video.day}</p><p>{video.song} by {video.artist}</p></span>;
+            video.description = <block><p>Day {video.day}</p><p>{video.song} by {video.artist}</p></block>;
           }
           return <Box key={video.id} {...video} />
         })}
@@ -94,14 +119,63 @@ var Songs = React.createClass({
         {this.state.videos.map(function(video) {
           video.description = <p>{video.song} by {video.artist}</p>;
           video.anchorUrl = '/song/'+video.song;
-          return <Box {...video} />
+          return <Box key={video.id} {...video} />
         })}
       </div>
     )
   }
+});
 
+var Artists = React.createClass({ 
+  loadVideos: function() {
+    if (videos.length == 0) {
+      Ajax.get('/api/videos').then(function(response) {
+        this.setState({videos: response});
+      }.bind(this));
+    }
+  },
+  getInitialState: function() {
+    return {videos: []};
+  },
+  componentDidMount: function() { 
+    this.loadVideos();
+  },
+  render: function() {
+    artists = extractUnique(this.state.videos, "artist");
+    console.log(artists);
+    return (
+      <div id="content">
+        {artists.map(function(artist, i) {
+          return (
+            <div className='text-list'><Link to='artists'>{artist}</Link></div>
+          )
+        })}
+      </div>
+    )
+  }
+});
 
+var Artist = React.createClass({
+  loadVideos: function() {
+    if (videos.length == 0) {
+      Ajax.get('/api/videos').then(function(response) {
+        this.setState({videos: response});
+      }.bind(this));
+    }
+  },
+  getInitialState: function() {
+    return {videos: []};
+  },
+  componentDidMount: function() { 
+    this.loadVideos();
+  },
+  render: function() {
+    return (
+      test
+      
 
+    );
+  }
 });
 
 var HomePage = React.createClass({
@@ -120,34 +194,14 @@ var HomePage = React.createClass({
   }
 });
 
-var SongPage = React.createClass({
-  render: function() {
-    return (
-      <html>
-        <body>
-          <Header />
-          <div id="container">
-            <Nav />
-            <Songs />
-These are songs.
-          </div>
-        </body>
-      </html>
-    );
-  }
-});
-
-/*
-React.render(
-  <HomePage />,
-  document.getElementById('example')
-);
-*/
-
 var routes = (
   <Route handler={HomePage}>
     <DefaultRoute handler={Videos}/>
     <Route name="songs" handler={Songs}/>
+    <Route name="home" handler={Videos}/>
+    <Route name="days" handler={HomePage}/>
+    <Route name="artists" handler={Artists}/>
+    <Route name="artist" handler={Artist}/>
   </Route>
 );
 
